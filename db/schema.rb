b454d3d536a7ca96e3cbb46367d6d6f985b83362
Giulia Menin <giulia.menin@gmail.com>
2023-07-15 17:39:29 +0900

    changed status from string to integer

--- db/schema.rb
+++ db/schema.rb
@@ -10,17 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_07_15_083145) do
+ActiveRecord::Schema[7.0].define(version: 2023_07_15_083539) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "bookings", force: :cascade do |t|
     t.date "date"
-    t.string "status"
     t.bigint "job_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "user_id", null: false
+    t.integer "status"
     t.index ["job_id"], name: "index_bookings_on_job_id"
     t.index ["user_id"], name: "index_bookings_on_user_id"
   end
