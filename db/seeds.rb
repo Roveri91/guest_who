507b2ce738a13b843b934aca8a963f36b7d2a064
Simone Roveri <spikeroveri91@gmail.com>
2023-07-18 20:15:43 +0900

    install Faker Gem

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+names = ["Flower girl/boy", "Priest", "Best man", "Bridesmaid", "Drunked Uncle", "GranPa", "GranMa", "Charioteer", "General guest"]
+# Faker::Lorem.sentences(number: 2)
+locations = ["Tokyo", "Osaka", "Hiroshima", "Kyoto", "Kobe", "Kanazawa", "Sapporo", "Naha", "Nagasaki", "Amami"]
+
