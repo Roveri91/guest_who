75b6bb824f92b53eee0c54460cba5c1c9114ed76
Simone Roveri <spikeroveri91@gmail.com>
2023-07-18 21:08:43 +0900

    completed seed with 4 Users and 20 Jobs

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,39 @@
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
 
+puts "Destroying all the USERS"
+User.delete_all
+
+users = []
+puts "Creating new Fabolous Users"
+
+user = User.create!(first_name: "Giulia", last_name: "Menin", email: "GiuliaMenin@gmail.com", password: "123456")
+users << user
+puts "created #{user.first_name} user"
+
+user = User.create!(first_name: "Tenny", last_name: "Ma", email: "TennyMa@gmail.com", password: "123456")
+users << user
+puts "created #{user.first_name} user"
+
+user = User.create!(first_name: "Jonny", last_name: "Smith", email: "JonnySmith@gmail.com", password: "123456")
+users << user
+puts "created #{user.first_name} user"
+
+user = User.create!(first_name: "Simone", last_name: "Roveri", email: "SimoneRoveri@gmail.com", password: "123456")
+users << user
+puts "created #{user.first_name} user"
+
+
 names = ["Flower girl/boy", "Priest", "Best man", "Bridesmaid", "Drunked Uncle", "GranPa", "GranMa", "Charioteer", "General guest"]
-# Faker::Lorem.sentences(number: 2)
 locations = ["Tokyo", "Osaka", "Hiroshima", "Kyoto", "Kobe", "Kanazawa", "Sapporo", "Naha", "Nagasaki", "Amami"]
 
+puts "Destroying all the JOBS ADV"
+Job.delete_all
+
+puts "Creating 20 new Jobs"
+20.times do
+  job = Job.create!(name: names.sample, price: rand(5...10)*1000, location: locations.sample, description: Faker::Lorem.sentences(number: 3), user: users.sample)
+  puts "create #{job.name} adv"
+end
+
+puts "Done"
