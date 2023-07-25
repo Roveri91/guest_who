678a1c414158b1738c7c5b1aaf2a00b01102a42a
Tenny Ma <128113562+tentenma@users.noreply.github.com>
2023-07-25 20:56:29 +0900

    search bar case insensitive

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -1,6 +1,12 @@
 class JobsController < ApplicationController
   def index
-    @jobs = Job.all
+    def index
+      if params[:query].present?
+        @jobs = @jobs.where("title ILIKE ?", "%#{params[:query]}%") # I stands for case insensitive
+      else
+        @jobs = Job.all
+      end
+    end
   end
 
   def show
@@ -16,7 +22,7 @@ class JobsController < ApplicationController
   end
 
   private
-  
+
   def job_params
     params.require(:job).permit(:name, :price, :description, :location, :user_id)
   end
