fe24c02923bf351396f023c3a0318c6e9d41ee35
Simone Roveri <spikeroveri91@gmail.com>
2023-07-27 20:36:43 +0900

    almost fixed, need to sort out why pic is not uploaded successfully

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -12,5 +12,25 @@ class JobsController < ApplicationController
     @job = Job.find(params[:id])
     @booking = Booking.new
   end
+  
+  def new
+    @job = Job.new
+  end
+
+  def create
+    @job = Job.new(job_params)
+    @job.user = current_user
+    if @job.save!
+      redirect_to provider_jobs_path
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def job_params
+    params.require(:job).permit(:name, :price, :description, :location, :user_id, :photo)
+  end
 
 end
