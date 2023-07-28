195cff43ef4523fa40b3b9581471be3fd2318ba3
Simone Roveri <spikeroveri91@gmail.com>
2023-07-28 16:22:41 +0900

    set a default pics if not insert in the form

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -1,3 +1,5 @@
+require "open-uri"
+
 class JobsController < ApplicationController
   # layout "job"
 
@@ -21,6 +23,10 @@ class JobsController < ApplicationController
     @job = Job.new(job_params)
     @job.user = current_user
     if @job.save!
+      if !@job.photo.attached?
+        file = URI.open("https://images.unsplash.com/photo-1456272875769-b47e860f93a4?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=874&q=80")
+        @job.photo.attach(io: file, filename: "jobphoto.jpg", content_type: "image/png")
+      end  
       redirect_to provider_jobs_path
     else
       render :new, status: :unprocessable_entity
